d9ea71c78a03f3588bc98d29bf33d7f9fb2c43d1
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-15 17:50:00 -0300

    Teste de Unidade - Classe AdminAutores com inclusão de método com anotação Before

--- teste/br/com/edigi/cadastro/AdminAutoresTest.java
+++ teste/br/com/edigi/cadastro/AdminAutoresTest.java
@@ -1,6 +1,7 @@
 package br.com.edigi.cadastro;
 
 import br.com.edigi.modelo.Autor;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,19 +9,27 @@ import static org.junit.Assert.assertTrue;
 
 public class AdminAutoresTest {
 
+    private AdminAutores lista;
+    private Autor cassio;
+    private Autor murilo;
+    private Autor santos;
+
+    @Before
+    public void criaAdminAutoresEAutor() {
+        this.lista = new AdminAutores();
+        this.cassio = new Autor("Cássio", "cassio@alura.com.br");
+        this.murilo = new Autor("Murilo", "murilo@alura.com.br");
+        this.santos = new Autor("Santos", "murilo@alura.com.br");
+    }
+
     @Test
     public void autoresDevemConstarNaLista() {
-
-        AdminAutores lista = new AdminAutores();
-
-        Autor cassio = new Autor("Cassio", "cassio@alura.com.br");
-        Autor murilo = new Autor("Murilo", "murilo@alura.com.br");
 
         lista.insereAutor(cassio);
         lista.insereAutor(murilo);
 
         assertEquals(2, lista.getListaDeAutores().size());
-        assertTrue("Cassio", lista.getListaDeAutores().contains(cassio));
+        assertTrue("Cássio", lista.getListaDeAutores().contains(cassio));
         assertTrue("cassio@alura.com.br", lista.getListaDeAutores().contains(cassio));
         assertTrue("Murilo", lista.getListaDeAutores().contains(murilo));
         assertTrue("murilo@alura.com.br", lista.getListaDeAutores().contains(murilo));
@@ -28,12 +37,6 @@ public class AdminAutoresTest {
 
     @Test(expected = RuntimeException.class)
     public void autoresNaoPodemTerOMesmoEmail() {
-
-        AdminAutores lista = new AdminAutores();
-
-        Autor cassio = new Autor("Cassio", "cassio@alura.com.br");
-        Autor murilo = new Autor("Murilo", "murilo@alura.com.br");
-        Autor santos = new Autor("Santos", "murilo@alura.com.br");
 
         lista.insereAutor(cassio);
         lista.insereAutor(murilo);
