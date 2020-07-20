8cbb958db06f13fcb9c22ba710b9d84f66ad3979
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-20 18:17:49 -0300

    Teste de Unidade - Classe AdminCategoria com alteração em categoriasDevemConstarNaLista

--- teste/br/com/edigi/cadastro/AdminCategoriaTest.java
+++ teste/br/com/edigi/cadastro/AdminCategoriaTest.java
@@ -4,19 +4,18 @@ import br.com.edigi.modelo.Categoria;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AdminCategoriaTest {
 
-    private AdminCategoria lista;
+    private AdminCategoria listaDeCategoria;
     private Categoria programacao;
     private Categoria frontend;
     private Categoria mobile;
 
     @Before
     public void criaCategoria() {
-        this.lista = new AdminCategoria();
+        this.listaDeCategoria = new AdminCategoria();
         this.programacao = new Categoria("Programação");
         this.frontend = new Categoria("Front=End");
         this.mobile = new Categoria("Programação");
@@ -25,20 +24,19 @@ public class AdminCategoriaTest {
     @Test
     public void categoriasDevemConstarNaLista() {
 
-        lista.insereCategoria(programacao);
-        lista.insereCategoria(frontend);
+        listaDeCategoria.insereCategoria(programacao);
+        listaDeCategoria.insereCategoria(frontend);
 
-        assertEquals(2, lista.getListaDeCategoria().size());
-        assertTrue("Programação", lista.getListaDeCategoria().contains(programacao));
-        assertTrue("Front-End", lista.getListaDeCategoria().contains(frontend));
+        assertTrue(String.valueOf(programacao), listaDeCategoria.getListaDeCategoria().contains(programacao));
+        assertTrue(String.valueOf(frontend), listaDeCategoria.getListaDeCategoria().contains(frontend));
     }
 
     @Test(expected = RuntimeException.class)
     public void categoriasNaoPodemTerOMesmoNome() {
 
-        lista.insereCategoria(programacao);
-        lista.insereCategoria(frontend);
-        lista.insereCategoria(mobile);
+        listaDeCategoria.insereCategoria(programacao);
+        listaDeCategoria.insereCategoria(frontend);
+        listaDeCategoria.insereCategoria(mobile);
 
     }
 
